88126c7902fe268e16a05785ba62156914be6d39
ikechukwufavour220-cloud <ikechukwufavour220@gmail.com>
2025-12-17 12:39:09 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', function(e) {
+  console.log('Service Worker: Installed');
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('Service Worker: Activated');
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
