6c62501b7763ca3d5d9effd3445a8717e7540d93
ikechukwufavour220-cloud <ikechukwufavour220@gmail.com>
2025-12-17 13:11:32 +0100

    Refactor service worker for caching and fetching

--- sw.js
+++ sw.js
@@ -1,13 +1,42 @@
-self.addEventListener('install', function(e) {
-  console.log('Service Worker: Installed');
+const cacheName = 'wealthwise-cache-v1';
+const assetsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/sw.js'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', function(e) {
-  console.log('Service Worker: Activated');
+// Activate Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+// Fetch cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(res => {
+        const resClone = res.clone();
+        caches.open(cacheName).then(cache => cache.put(event.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(event.request).then(res => res))
   );
 });
